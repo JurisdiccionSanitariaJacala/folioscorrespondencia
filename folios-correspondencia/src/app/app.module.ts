1a6caa5fc12bbf716d8319e8a31e1ccede5392e7
Christian Yamil Castillo <cyccov@gmail.com>
2021-09-09 10:28:39 -0500

    -

--- folios-correspondencia/src/app/app.module.ts
+++ folios-correspondencia/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FoliosComponent } from './folios/folios.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { FoliosComponent } from './folios/folios.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
