45e6db528781eb4ba5d514a9e897b719450e1160
Christian Yamil Castillo <cyccov@gmail.com>
2021-09-09 10:12:06 -0500

    -
    Added bootstrap

--- folios-correspondencia/src/app/app.module.ts
+++ folios-correspondencia/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FoliosComponent } from './folios/folios.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FoliosComponent
   ],
   imports: [
     BrowserModule,
